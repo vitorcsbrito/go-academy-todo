3435b85b49f99c7c611d37f3b89c461beb555f9a
Vitor Brito <vitor.brito1@specsavers.com>
2024-07-30 18:18:06 +0100

    templating

--- app.go
+++ app.go
@@ -4,7 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -29,7 +36,7 @@ func writeTasksToJsonFile(filename string, todos ...ToDo) {
 	writeFile(filename, string(jsonStr))
 }
 
-func readTasksFromJson(filename string, todos []ToDo) {
+func readTasksFromJson(filename string, todos *[]ToDo) {
 	var tasks []ToDo
 	var content = readJsonFile(filename)
 
@@ -38,7 +45,7 @@ func readTasksFromJson(filename string, todos []ToDo) {
 		log.Fatal(err)
 	}
 
-	todos = tasks
+	*todos = append(*todos, tasks...)
 
 	printTodosFormatted(tasks)
 }
@@ -70,33 +77,115 @@ func findTask(id int) (ToDo, int, error) {
 
 var tasks []ToDo
 
-func main() {
-	tasks = append(tasks,
-		ToDo{0, "do dishes", false},
-		ToDo{1, "do laundry", false})
+// createTask adds an album from JSON received in the request body.
+func createTask(c *gin.Context) {
+	var newTask ToDo
 
+	// Call BindJSON to bind the received JSON to
+	// newTask.
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	newTask.Id = findLatestId()
+
+	tasks = append(tasks, newTask)
+	writeTasksToJsonFile("tasks.json", tasks...)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
+func updateTask(c *gin.Context) {
+	var newValues ToDo
+	// Call BindJSON to bind the received JSON to
+	// newValues.
+	if err := c.BindJSON(&newValues); err != nil {
+		return
+	}
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	_, i, _ := findTask(id)
+
+	tasks[i].Description = newValues.Description
+	tasks[i].Done = newValues.Done
+
+	//tasks = append(tasks, newValues)
+	writeTasksToJsonFile("tasks.json", tasks...)
+	c.IndentedJSON(http.StatusCreated, tasks[i])
+}
+
+func findLatestId() int {
+	sort.SliceStable(tasks, func(i, j int) bool {
+		return tasks[i].Id > tasks[j].Id
+	})
+
+	return tasks[0].Id + 1
+}
+
+func getTaskById(c *gin.Context) {
+	id := c.Param("id")
+	i, _ := strconv.Atoi(id)
+
+	for _, a := range tasks {
+		if a.Id == i {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "task not found"})
+}
+
+func main() {
 	filename := "tasks.json"
 
-	ch := make(chan []ToDo)
+	readTasksFromJson(filename, &tasks)
 
-	writeTasksToJsonFile(filename, tasks...)
-	readTasksFromJson(filename, tasks)
+	router := gin.Default()
 
-	go updateTaskDescription(1, "one", ch)
-	<-ch
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "tasks.tmpl", getSortedTasks())
+	})
 
-	go updateTaskDescription(1, "two", ch)
-	<-ch
+	//router.GET("/tasks", getTasks)
+	router.GET("/tasks/:id", getTaskById)
+	router.POST("/tasks", createTask)
+	router.PUT("/tasks/:id", updateTask)
 
-	go updateTaskDescription(1, "three", ch)
-	<-ch
+	err := router.Run(":8080")
+	if err != nil {
+		return
+	}
+}
 
-	go updateTaskDescription(1, "four", ch)
-	<-ch
+func getSortedTasks() []ToDo {
+	sort.SliceStable(tasks, func(i, j int) bool {
+		return tasks[i].Id < tasks[j].Id
+	})
 
-	time.Sleep(50 * time.Millisecond)
+	return tasks
+}
 
-	updatedTask, _, _ := findTask(1)
-
-	println(updatedTask.Description)
+func genTemplate() {
+	funcMap := template.FuncMap{
+		"dec":     func(i int) int { return i - 1 },
+		"replace": strings.ReplaceAll,
+	}
+	var tmplFile = "tasks.tmpl"
+	tmpl, err := template.New(tmplFile).Funcs(funcMap).ParseFiles(tmplFile)
+	if err != nil {
+		panic(err)
+	}
+	var f *os.File
+	f, err = os.Create("pets.html")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(f, tasks)
+	if err != nil {
+		panic(err)
+	}
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
 }
---
+++ routes/getTasks.go
@@ -0,0 +1,11 @@
+package routes
+
+//import (
+//	"github.com/gin-gonic/gin"
+//	"net/http"
+//)
+//
+//// getTasks responds with the list of all albums as JSON.
+//func getTasks(c *gin.Context, tasks []ToDo) {
+//	c.IndentedJSON(http.StatusOK, tasks)
+//}
